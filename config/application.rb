5f448301708ae517412f15c123c58534939e8b51
tsubasaozawa <tsubasaozawa@users.noreply.github.com>
2019-12-07 02:36:45 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module TechMatch
   class Application < Rails::Application
+    config.load_defaults 5.2
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
