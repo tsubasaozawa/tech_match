6c48732247ba67c71f1b04d99489d17d3e698340
tsubasaozawa <tsubasaozawa@users.noreply.github.com>
2019-12-08 15:38:52 +0900

    ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
   resources :users do
+    get 'users/:name', controller: 'users', action: 'edit'
     collection do
       get :admin_member
+      get :follower_list
+      get :following_list
+      get :matching_list
     end
     member do
       get :withdrawal
